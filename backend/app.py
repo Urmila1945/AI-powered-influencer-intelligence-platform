ffe22e92bd00192c2e3b27fc3f628a313be13959
Urmila Kshirsagar <urmilakshirsagar1945@gmail.com>
2026-06-03 09:01:28 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -62,5 +62,11 @@ def create_app():
 # Instantiate app for Gunicorn
 app = create_app()
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=config.DEBUG)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get("PORT", 5000)),
+        debug=config.DEBUG
+    )
